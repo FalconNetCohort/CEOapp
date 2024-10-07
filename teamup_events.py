f3f9973c478aeac18175c7b704b8e6d88fa8c039
Tomis Hamilton <tomisnhamilton@gmail.com>
2024-10-07 08:15:43 -0600

    Removed API Key from Public Scope
    Don't post APIs in clear-text

--- teamup_events.py
+++ teamup_events.py
@@ -1,5 +1,5 @@
 import requests
-apikey = "021d5ce3f150bda7e1727d637ed9ccdc1a40d4c03a5f693721ee5badc39c18a7"
+apikey = "REDACTED"
 
 session = requests.Session()
 #session.auth = ('Teamup-Token ', apikey);
