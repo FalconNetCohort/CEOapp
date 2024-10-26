97b7de0f970b97134b61f11da004654703fe8807
Mason C <masonneon@gmail.com>
2024-10-26 09:24:26 -0600

    Testing post requests

--- teamup_events.py
+++ teamup_events.py
@@ -1,10 +1,22 @@
 import requests
-apikey = "REDACTED"
+import config
 
 session = requests.Session()
-#session.auth = ('Teamup-Token ', apikey);
-session.headers.update({'Teamup-Token': apikey})
+session.headers.update(
+        {
+            'Teamup-Token': config.TEAMUP_API_KEY,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    )
 
-r = session.get('https://api.teamup.com/ks9q9tx6hkpjiictdg/events')
+payload = {
+        'subcalendar_ids':[0],
+        'start_dt':'2024-10-26T14:35:24Z', 
+        'end_dt':'2024-10-26T15:35:24Z',
+        'title': 'test',
+           }
+r = session.post('https://api.teamup.com/ksvvx3e1n68gmkz7x8/events', json=payload)
+
 print(r.status_code)
 print(r.text)
